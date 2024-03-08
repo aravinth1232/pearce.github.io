cf70ce8b9679857781aa32bb3a191b523dc72d0f
aravinth1232 <aravinthatr12@gmail.com>
2024-03-08 11:34:28 +0530

    files added

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+const open = document.getElementById("open")
+const close = document.getElementById("close")
+
+const side = document.getElementById("sidemenu")
+
+function on(){
+    side.style.display ="flex"
+    open.style.display = "none"
+
+}
+
+function off(){
+    side.style.display ="none"
+    open.style.display = "flex"
+}
