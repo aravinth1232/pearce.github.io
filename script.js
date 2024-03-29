def96c74aa7bcfb84ed809fca564cd4d786d32dc
aravinth1232 <aravinthatr12@gmail.com>
2024-03-29 15:55:48 +0530

    files added

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ const close = document.getElementById("close")
 const side = document.getElementById("sidemenu")
 
 function on(){
-    side.style.display ="flex"
-   
+    // side.style.display ="flex"
+    side.style.left ="0%"
 
-    open.style.display = "none"
+    // open.style.display = "none"
 
 }
 
 function off(){
-    side.style.display ="none"
+    // side.style.display ="none"
     open.style.display = "flex"
+    side.style.left ="-100%"
 }
 
 
 
 
-let search = document.getElementById("search")
